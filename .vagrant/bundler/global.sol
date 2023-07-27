5297adb55fd9f073f0f9a0f5cb4f00c220959f94
a.mazzocchi <a.mazzocchi@cineca.it>
2023-07-27 18:22:29 +0200

    Update .gitignore

--- .vagrant/bundler/global.sol
+++
@@ -1,1 +0,0 @@
-{"dependencies":[["net-ssh",[">= 2.6.5","< 7.0.0"]],["net-scp",[">= 1.1"]],["log4r",["~> 1.1"]],["vagrant-scp",["= 0.5.9"]],["concurrent-ruby",["~> 1.0"]],["i18n",[">= 0"]],["micromachine",[">= 2","< 4"]],["vagrant-vbguest",["= 0.30.0"]]],"checksum":"9ab812748b1dedcce78ff678a40e66adf2a92fec58761e2afa1897f91cd09011","vagrant_version":"2.2.19"}
