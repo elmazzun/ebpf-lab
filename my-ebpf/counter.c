4e2bd5614e0f92f34acd3175d2bbb7f231c2a694
a.mazzocchi <a.mazzocchi@cineca.it>
2024-11-17 17:27:10 +0100

    Add missing ETH_P_IP definition from vmlinux.h

--- my-ebpf/counter.c
+++ my-ebpf/counter.c
@@ -1,5 +1,8 @@
 //go:build ignore
 
+// Missing definition from vmlinux.h
+#define ETH_P_IP	0x0800
+
 #include "vmlinux.h"
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
@@ -11,13 +14,38 @@ struct {
     __uint(max_entries, 1);
 } pkt_count SEC(".maps"); 
 
-// count_packets atomically increases a packet counter on every invocation.
 SEC("xdp") 
-int count_packets() {
+int count_packets(struct xdp_md *ctx) {
     __u32 key    = 0; 
-    __u64 *count = bpf_map_lookup_elem(&pkt_count, &key); 
-    if (count) { 
-        __sync_fetch_and_add(count, 1); 
+    __u64 *count = bpf_map_lookup_elem(&pkt_count, &key);
+
+   if (count) {
+       __sync_fetch_and_add(count, 1);
+   }
+
+    void *data = (void *)(long)ctx->data;         // start of packet data
+    void *data_end = (void *)(long)ctx->data_end; // end of packet data
+
+    struct ethhdr *eth = data;
+    if (data + sizeof(struct ethhdr) > data_end) {
+        bpf_printk("Invalid Ethernet header, drop packet\n");
+	return XDP_DROP;
+    }
+
+    if (bpf_ntohs(eth->h_proto) != ETH_P_IP) {
+        //bpf_printk("Ethernet not IPv4, dropping\n");
+	return XDP_DROP;
+    }
+
+    struct iphdr *iph = data + sizeof(struct ethhdr);
+    if (data + sizeof(struct ethhdr) + sizeof(struct iphdr) > data_end) {
+        bpf_printk("Invalid IP header, drop packet\n");
+	return XDP_DROP;
+    }
+
+    if (iph->protocol == IPPROTO_ICMP) {
+        bpf_printk("Found ICMP, drop packet\n");
+	return XDP_DROP;
     }
 
     return XDP_PASS; 
